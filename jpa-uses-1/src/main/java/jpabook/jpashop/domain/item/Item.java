97b135a49791cb1acce2577f77b9b09783cf7018
kimyezzang97 <kimyezzang97@gmail.com>
2024-04-02 12:13:45 +0900

    도메인 분석 설계 - 엔티티 클래스 개발2 keep

--- jpa-uses-1/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpa-uses-1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.bytecode.enhance.spi.EnhancementInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -19,4 +23,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
